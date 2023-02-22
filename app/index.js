407f1883c635bc51bcb565f810123298b3ef1f0a
Yahya <yahya.muhammad.356a@gmail.com>
2023-02-22 17:45:10 -0500

    first commit

---
+++ app/index.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+var task = [];
+var complete = [];
+
+app.post("/addtask", function (req, res) {
+  var newTask = req.body.newtask;
+  task.push(newTask);
+  res.redirect("/");
+});
+app.post("/removetask", function (req, res) {
+  var completeTask = req.body.check;
+  if (typeof completeTask === "string") {
+    complete.push(completeTask);
+    task.splice(task.indexOf(completeTask), 1);
+  } else if (typeof completeTask === "object") {
+    for (var i = 0; i < completeTask.length; i++) {
+      complete.push(completeTask[i]);
+      task.splice(task.indexOf(completeTask[i]), 1);
+    }
+  }
+  res.redirect("/");
+});
+app.get("/", function (req, res) {
+  res.render("index", { task: task, complete: complete });
+});
+app.listen(5000, function () {
+  console.log("Running on port 5000!");
+});
